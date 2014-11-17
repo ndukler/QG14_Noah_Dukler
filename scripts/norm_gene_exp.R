6b6a7161a39973c630ae3dfc841562bcb8536498
Noah Dukler <ned36@conrell.edu>
2014-11-16 23:33:10 -0500

    Full project setup and plink analysis scripts functional.

--- scripts/norm_gene_exp.R
+++ scripts/norm_gene_exp.R
@@ -1,12 +1,19 @@
-#!/usr/bin/env Rscript
+args = commandArgs(trailingOnly = TRUE)
+setwd(args[1])
+dir()
 
-library(GenABEL)
+libpath=paste0(args[1],"/lib/") 
+.libPaths(new = libpath)
+if(!require("GenABEL")){
+  install.packages("GenABEL",lib = libpath, repos='http://cran.us.r-project.org')
+}
 
+library("GenABEL")
 phenotypes = read.table("processed_files/gene_expression.phe",header = T)
 
 phenotypes$MRPL40 <- rntransform(phenotypes$MRPL40)
 phenotypes$GGT5 <- rntransform(phenotypes$GGT5)
 phenotypes$TTC38 <- rntransform(phenotypes$TTC38)
 
-write.table(out, "processed_files/norm_gene_expression.phe",quote=F,row.names = F)
+write.table(phenotypes, "processed_files/norm_gene_expression.phe",quote=F,row.names = F)
 
