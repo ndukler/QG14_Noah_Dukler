87fc7c0565372b433e7572def0a04a7f994f3413
Noah Dukler <ned36@conrell.edu>
2014-11-19 12:33:04 -0500

    Beginning complete rework of makefile...better understanding

--- scripts/initialize_project.sh
+++ scripts/initialize_project.sh
@@ -1,15 +1,21 @@
 #!/usr/bin/env bash
 
-mkdir -p $1
+home=`pwd`
 
 cd $1
 mkdir -p downloads
 mkdir -p lib
 
 cd downloads
-wget http://mezeylab.cb.bscb.cornell.edu/labmembers/documents/QG14_project_genotypes.zip 
-wget http://mezeylab.cb.bscb.cornell.edu/labmembers/documents/QG14_project_phenotypes.txt
-wget http://mezeylab.cb.bscb.cornell.edu/labmembers/documents/QG14_project_covariates.txt
 
-unzip -n QG14_project_genotypes.zip 
-rm QG14_project_genotypes.zip 
+while read file; do
+    echo $file
+    wget -N $file
+    if [[ $file =~ \.zip$ ]]; then
+	base=`basename $file`
+	unzip -o $base
+    fi
+done < <(sed 's/\s\+//g' $home/$2 | cut -d "=" -f2)
+
+
+
